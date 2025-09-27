fb51df187106ea9cdfa3f5a151a5ecedd91fe920
Martin Wyatt <martin.wyatt@gmail.com>
2025-09-27 20:30:45 +0100

    Revert "fix: trial memory optimisation for build"
    This reverts commit fcb13ade733f1c2a2116acce403cf12ced692cf3.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    webpackMemoryOptimizations: true
-  }
+  output: 'standalone'
 }
 
 export default nextConfig
