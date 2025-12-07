38a7c6a66dceb3e3677123357c30965e68f02620
Martin Wyatt <martin.wyatt@gmail.com>
2025-12-07 20:41:42 +0000

    New server (#8)
    * fix: new server
    
    * fix: checks

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -8,11 +8,12 @@ export default function Login () {
   return (
     <div className='flex flex-row min-h-screen justify-center items-center pb-14'>
       <div className='border border-tertiary-500 p-6 rounded'>
-        <div className='w-20 mx-auto mb-6'>
+        <div className='w-20 mx-auto mb-2'>
           <Link href='/'>
             <ElttlEmblem />
           </Link>
         </div>
+        <div className='text-xs text-stone-400 text-center mb-4'>Version {process.env.NEXT_CURRENT_VERSION}</div>
         <LoginForm />
       </div>
     </div>
