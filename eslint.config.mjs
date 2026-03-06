87563be5ce16ac82f534759eced31c848eeb455e
Martin Wyatt <martin.wyatt@gmail.com>
2026-03-06 16:01:03 +0000

    feat: init storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,34 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook'
+
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import pluginUnusedImports from 'eslint-plugin-unused-imports'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  {
-    plugins: {
-      'unused-imports': pluginUnusedImports
-    },
-    rules: {
-      'no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        {
-          vars: 'all',
-          varsIgnorePattern: '^_',
-          args: 'after-used',
-          argsIgnorePattern: '^_'
-        }
-      ]
-    }
+const eslintConfig = defineConfig([...nextVitals, {
+  plugins: {
+    'unused-imports': pluginUnusedImports
   },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts'
-  ])
-])
+  rules: {
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ]
+  }
+}, // Override default ignores of eslint-config-next.
+globalIgnores([
+  // Default ignores of eslint-config-next:
+  '.next/**',
+  'out/**',
+  'build/**',
+  'next-env.d.ts'
+]), ...storybook.configs['flat/recommended']])
 
 export default eslintConfig
