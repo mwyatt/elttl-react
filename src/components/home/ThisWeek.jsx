06b69b4ec5a04c36e6140f33badeae62efd140f8
Martin Wyatt <martin.wyatt@gmail.com>
2026-03-27 19:34:22 +0000

    Storybook (#19)
    * feat: init storybook
    
    * feat: this week stories, added no week variation
    
    * fix: tidy

--- src/components/home/ThisWeek.jsx
+++ src/components/home/ThisWeek.jsx
@@ -4,6 +4,7 @@ import GeneralLink from '@/components/GeneralLink'
 import { allHomeButtonStyles } from '@/lib/styles'
 
 export default function ThisWeek ({ yearName, week, fixtures = [] }) {
+  const description = (fixtures.length > 0 ? `${fixtures.length} fixture${fixtures.length > 1 ? 's' : ''} are` : 'There are no fixtures') + ' scheduled to be played this week'
   return (
     <div className='grow flex flex-col gap-4 h-full'>
       <div className='flex w-full justify-end'>
@@ -11,13 +12,14 @@ export default function ThisWeek ({ yearName, week, fixtures = [] }) {
         <GeneralLink className={allHomeButtonStyles} href={`/result/${yearName}/season`}>Season Overview</GeneralLink>
       </div>
       <div className='grow flex items-center text-center'>
-        {fixtures.length > 0 && (
-          <p className='mt-2 text-2xl'>{fixtures.length} fixture{fixtures.length > 1 ? 's' : ''} are scheduled to be played this week</p>
-        )}
+        <p className='mt-2 text-2xl'>{description}</p>
       </div>
-      <div className='flex justify-end'>
-        <GeneralLink href={`/result/${yearName}/week/${week.id}`} className='bg-primary-500 rounded px-3 py-2 text-white font-bold capitalize transition-colors text-lg'>View Week</GeneralLink>
-      </div>
+      {week && (
+
+        <div className='flex justify-end'>
+          <GeneralLink href={`/result/${yearName}/week/${week.id}`} className='bg-primary-500 rounded px-3 py-2 text-white font-bold capitalize transition-colors text-lg'>View Week</GeneralLink>
+        </div>
+      )}
     </div>
   )
 }
